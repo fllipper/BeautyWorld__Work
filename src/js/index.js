afb3f25293d587aaeea3c54122b7bbc4518bf8c1
Fllipper <homegor14@gmail.com>
2021-12-18 02:31:23 +0700

    commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.works').slick({
+        prevArrow: 'work__btn-prev',
+        nextArrow: 'work__btn-next',
+        slidesToShow: 4,
+        infinite: false
+    });
+});
